7e645d2105826840d728f2b31014c057d8602464
victory studio <hello@victory.studio>
2020-03-04 23:07:45 +0200

    анимация при наведении на пункты меню на vue

--- src/js/index.js
+++ src/js/index.js
@@ -48,9 +48,35 @@ new Vue({
 
 
 var navLine = new Vue ({
-  el: '#js-nav',
+  el: '#js-nav-line',
   data: {
-    message: 'hello'
+    isActive: {
+      'header__line_about': false,
+      'header__line_portfolio': false,
+      'header__line_service': false,
+      'header__line_contacts': false
+    },
+    lineConst: ''
+  },
+  methods: {
+    click: function(el) {
+      this.lineConst = el;
+      this.clear();
+      this.isActive[this.lineConst] = true;
+    },
+    hover: function(el) {
+      this.clear();
+      this.isActive[el] = true;
+    },
+    leave: function(){
+      this.clear();
+      this.isActive[this.lineConst] = true;
+    },
+    clear: function() {
+      for (var key in this.isActive) {
+        this.isActive[key] = false;
+      };
+    }
   }
 })
 
